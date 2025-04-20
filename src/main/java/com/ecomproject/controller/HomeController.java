2de5f3817d53a3364a499598fd3e83aec8078d57
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-20 11:19:18 +0530

    Update

--- src/main/java/com/ecomproject/controller/HomeController.java
+++ src/main/java/com/ecomproject/controller/HomeController.java
@@ -1,4 +1,14 @@
 package com.ecomproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping
+    public String HomeControllerHandler()
+    {
+        return "Welcome to My Ecommerce Project";
+    }
 }

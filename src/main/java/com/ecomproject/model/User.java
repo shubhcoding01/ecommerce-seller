f2f289aee4b823860cb5c7c8dc481dcbe01291c1
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-20 13:15:39 +0530

    Update

--- src/main/java/com/ecomproject/model/User.java
+++ src/main/java/com/ecomproject/model/User.java
@@ -1,11 +1,14 @@
 package com.ecomproject.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 }

b975613a427624271f6a4fff968a27abd7bbdaac
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-20 11:54:09 +0530

    Update the HomeController.java file to include a new method for handling user login.

--- src/main/java/com/ecomproject/EcommerceSellerApplication.java
+++ src/main/java/com/ecomproject/EcommerceSellerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSellerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EcommerceSellerApplication.class, args);
 	}
 

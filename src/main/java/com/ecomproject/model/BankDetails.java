7509578845248a6c7676298d78780944677fa6f2
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-24 14:49:26 +0530

    Update

--- src/main/java/com/ecomproject/model/BankDetails.java
+++ src/main/java/com/ecomproject/model/BankDetails.java
@@ -1,4 +1,13 @@
 package com.ecomproject.model;
 
+import lombok.Data;
+
+@Data
 public class BankDetails {
+
+    private String accountNumber;
+
+    private String accountHolderName;
+
+    private String ifscCode;
 }

7e4a94a37debb58bc4052dbf41faf0756098e925
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-24 14:52:26 +0530

    Update

--- src/main/java/com/ecomproject/model/BusinessDetails.java
+++ src/main/java/com/ecomproject/model/BusinessDetails.java
@@ -1,4 +1,14 @@
 package com.ecomproject.model;
 
+import lombok.Data;
+
+@Data
 public class BusinessDetails {
+
+    private String businessName;
+    private String businessAddress;
+    private String businessEmail;
+    private String businessMobile;
+    private String logo;
+    private String banner;
 }

8199ba5dafea95fc90bdec74713270ec40cd07c5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-24 14:07:52 +0530

    Update

--- src/main/java/com/ecomproject/model/Review.java
+++ src/main/java/com/ecomproject/model/Review.java
@@ -1,4 +1,22 @@
 package com.ecomproject.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    
 }

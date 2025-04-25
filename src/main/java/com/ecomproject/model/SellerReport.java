bdb8bea3141f9043cdc4defd2a96c31873a8d843
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-25 14:26:51 +0530

    Update

--- src/main/java/com/ecomproject/model/SellerReport.java
+++ src/main/java/com/ecomproject/model/SellerReport.java
@@ -1,9 +1,6 @@
 package com.ecomproject.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -18,5 +15,22 @@ public class SellerReport {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    
+    @OneToOne
+    private Seller seller;
+
+    private Long totalEarnings = 0L;
+
+    private Long totalSales = 0L;
+
+    private Long totalRefunds = 0L;
+
+    private Long totalTax =0L;
+
+    private Long netEarnings = 0L;
+
+    private Integer totalOrders = 0;
+
+    private Integer canceledOrders = 0;
+
+    private Integer totalTransactions = 0;
 }

22b02efdadfaf36fa754807349513d8c3b1ff972
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-28 22:33:20 +0530

    Update

--- src/main/java/com/ecomproject/controller/AuthController.java
+++ src/main/java/com/ecomproject/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.model.User;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+
+    public ResponseEntity<User> createUserHandler(){
+        return null;
+    }
 }

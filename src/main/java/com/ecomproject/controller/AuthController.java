0b3a5dd03f6348b5c1fdcdef04031bd756caa00d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-28 23:10:31 +0530

    Update

--- src/main/java/com/ecomproject/controller/AuthController.java
+++ src/main/java/com/ecomproject/controller/AuthController.java
@@ -1,23 +1,31 @@
 package com.ecomproject.controller;
 
 import com.ecomproject.model.User;
+import com.ecomproject.repository.UserRepository;
 import com.ecomproject.response.SignupRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
-    @PostMapping
+    private final UserRepository userRepository;
+
+    @PostMapping("signup")
     public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req){
 
         User user = new User();
         user.setEmail(req.getEmail());
         user.setFullName(req.getFullName());
 
-        return ResponseEntity.ok(user);
+        User savedUser=userRepository.save(user);
+
+        return ResponseEntity.ok(savedUser);
     }
 }

9768d85c77095748e3634155c76080bcf8efd8d4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-28 23:05:01 +0530

    Update

--- src/main/java/com/ecomproject/controller/AuthController.java
+++ src/main/java/com/ecomproject/controller/AuthController.java
@@ -3,12 +3,15 @@ package com.ecomproject.controller;
 import com.ecomproject.model.User;
 import com.ecomproject.response.SignupRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class AuthController {
 
+    @PostMapping
     public ResponseEntity<User> createUserHandler(@RequestBody SignupRequest req){
 
         User user = new User();

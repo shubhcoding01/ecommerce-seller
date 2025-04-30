c0ab9f1c05843e7d7dc802308eb4167ca54ea12b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-30 10:19:29 +0530

    Update

--- src/main/java/com/ecomproject/model/User.java
+++ src/main/java/com/ecomproject/model/User.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -44,8 +43,8 @@ public class User {
     private Set<Coupon> usedCoupons = new HashSet<>();
 
     // Add discount_percent as a field
-    @Column(precision = 5, scale = 2) // Optional: Adjust precision and scale as needed
-    private BigDecimal discountPercent = BigDecimal.ZERO; // Default value
+//    @Column(precision = 5, scale = 2) // Optional: Adjust precision and scale as needed
+//    private BigDecimal discountPercent = BigDecimal.ZERO; // Default value
 
 //    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
 //    @JsonIgnore

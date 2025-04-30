69f11be01f12c41286729fb8de82102cf2d4df50
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-30 16:13:08 +0530

    Update

--- src/main/java/com/ecomproject/config/JWT_CONSTANT.java
+++ src/main/java/com/ecomproject/config/JWT_CONSTANT.java
@@ -1,4 +1,6 @@
 package com.ecomproject.config;
 
 public class JWT_CONSTANT {
+    public static final String SECRET_KEY="ssssssssss";
+    public static final String JWT_HEADER="Authorization";
 }
--- src/main/java/com/ecomproject/config/JwtTokenValidator.java
+++ src/main/java/com/ecomproject/config/JwtTokenValidator.java
@@ -1,18 +1,38 @@
 package com.ecomproject.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.Filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
+        String jwt = request.getHeader("Authorization");
+
+//        Bearer jwt
+        if(jwt != null){
+            jwt=jwt.substring(7);
+            try{
+                SecretKey key = Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
+                Claims claims =Jwts.parser().setSigningKey(key).build().
+                        parseClaimsJws(jwt).getBody();
+
+            }
+            catch (Exception e){
+                throw new BadCredentialsException("Invalid token...!!!");
+            }
+        }
     }
 }

52ddb871b1c7abe5ce12f70f8f27c03cd41446f8
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-30 11:44:13 +0530

    Update

--- src/main/java/com/ecomproject/config/AppConfig.java
+++ src/main/java/com/ecomproject/config/AppConfig.java
@@ -1,26 +1,29 @@
 package com.ecomproject.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class AppConfig {
 
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
         http.sessionManagement(management->management.sessionCreationPolicy(
                 SessionCreationPolicy.STATELESS
         )).authorizeRequests(authorize->authorize
-                .requestMatchers("/api**").authenticated()
+                .requestMatchers("/api/**").authenticated()
                 .requestMatchers("/api/products/*/reviews").permitAll()
                 .anyRequest().permitAll()
-                .and().addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+        ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(csrf->csrf.disable())
-                .csrf(cors->cors.configure()))
+                .cors(cors->cors.configurationSource(corsConfigurationSource))
           return null;
     }
 }

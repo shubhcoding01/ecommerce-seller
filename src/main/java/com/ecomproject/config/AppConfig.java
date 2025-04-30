37acd9037c9714db0094d16453c8e580cd858a0d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-30 11:10:42 +0530

    Update

--- src/main/java/com/ecomproject/config/AppConfig.java
+++ src/main/java/com/ecomproject/config/AppConfig.java
@@ -1,7 +1,26 @@
 package com.ecomproject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class AppConfig {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.sessionManagement(management->management.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+        )).authorizeRequests(authorize->authorize
+                .requestMatchers("/api**").authenticated()
+                .requestMatchers("/api/products/*/reviews").permitAll()
+                .anyRequest().permitAll()
+                .and().addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf->csrf.disable())
+                .csrf(cors->cors.configure()))
+          return null;
+    }
 }

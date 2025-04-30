1a0f99a580689c427de97d18fd955e31eb0db74c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-30 19:11:20 +0530

    Update JwtProvider.java to use the new JWT library

--- src/main/java/com/ecomproject/config/JwtProvider.java
+++ src/main/java/com/ecomproject/config/JwtProvider.java
@@ -1,10 +1,23 @@
 package com.ecomproject.config;
 
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 
 import javax.crypto.SecretKey;
+import java.util.Collection;
 
 public class JwtProvider {
 
     SecretKey key = Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
+
+    public String generateToken(Authentication auth) {
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        String roles = populateAuthorities(authorities);
+        return null;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        return null;
+    }
 }

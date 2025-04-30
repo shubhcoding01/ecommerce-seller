af24770417f822cdd90a6b05c38897fbf45eadde
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-04-30 16:19:28 +0530

    Update

--- src/main/java/com/ecomproject/config/JwtTokenValidator.java
+++ src/main/java/com/ecomproject/config/JwtTokenValidator.java
@@ -10,10 +10,15 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.List;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
     @Override
@@ -28,6 +33,12 @@ public class JwtTokenValidator extends OncePerRequestFilter {
                 SecretKey key = Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
                 Claims claims =Jwts.parser().setSigningKey(key).build().
                         parseClaimsJws(jwt).getBody();
+
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+
+                List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null,auths);
 
             }
             catch (Exception e){

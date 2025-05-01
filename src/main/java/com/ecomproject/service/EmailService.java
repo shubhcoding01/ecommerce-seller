fae08aa9d9aa811b451f307f1fa7deea46445be1
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-01 13:48:15 +0530

    Update EmailService.java

--- src/main/java/com/ecomproject/service/EmailService.java
+++ src/main/java/com/ecomproject/service/EmailService.java
@@ -1,9 +1,12 @@
 package com.ecomproject.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailService {
+
+    private final JavaMailSender mailSender;
 }

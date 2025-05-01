840710f563c3f6b6a9da0849b39b65dbf0e36547
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-01 10:59:14 +0530

    Update

--- src/main/java/com/ecomproject/response/AuthResponse.java
+++ src/main/java/com/ecomproject/response/AuthResponse.java
@@ -1,7 +1,9 @@
 package com.ecomproject.response;
 
 import com.ecomproject.role.UserRole;
+import lombok.Data;
 
+@Data
 public class AuthResponse {
     private String jwt;
     private String message;

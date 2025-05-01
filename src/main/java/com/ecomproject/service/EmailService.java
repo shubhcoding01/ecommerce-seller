aa090e25014b7d35c2e25aae9fd3bd52f4ab97f0
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-01 14:00:05 +0530

    Update

--- src/main/java/com/ecomproject/service/EmailService.java
+++ src/main/java/com/ecomproject/service/EmailService.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,6 +18,8 @@ public class EmailService {
                                          String text) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(
+                    mimeMessage, "utf-8");
         }
         catch (MailException e){
             throw new MailSendException("Error sending verification OTP email") ;

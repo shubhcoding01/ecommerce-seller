13bffd3a87044e188a8a8c34b6c221e0d9a1f2ca
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-01 10:32:28 +0530

    Update

--- src/main/java/com/ecomproject/service/impl/AuthServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/AuthServiceImpl.java
@@ -2,7 +2,11 @@ package com.ecomproject.service.impl;
 
 import com.ecomproject.response.SignupRequest;
 import com.ecomproject.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     @Override
     public String createUser(SignupRequest req) {

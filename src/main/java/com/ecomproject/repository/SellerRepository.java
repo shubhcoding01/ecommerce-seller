38129b45cba9a336aa3c9d4010846b443a9c1bae
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-01 13:16:47 +0530

    Update JWT_CONSTANT.java, SellerRepository.java, CustomUserServiceImpl.java

--- src/main/java/com/ecomproject/repository/SellerRepository.java
+++ src/main/java/com/ecomproject/repository/SellerRepository.java
@@ -1,4 +1,8 @@
 package com.ecomproject.repository;
 
-public interface SellerRepository {
+import com.ecomproject.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+    Seller findByEmail(String email);
 }

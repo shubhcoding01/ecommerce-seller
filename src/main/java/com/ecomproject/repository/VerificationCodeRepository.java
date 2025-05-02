82109f48c186943b0d048da3f9b026d1aed5d053
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-02 10:31:25 +0530

    Update

--- src/main/java/com/ecomproject/repository/VerificationCodeRepository.java
+++ src/main/java/com/ecomproject/repository/VerificationCodeRepository.java
@@ -3,6 +3,6 @@ package com.ecomproject.repository;
 import com.ecomproject.model.VerificationCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Integer> {
-    
+public interface VerificationCodeRepository extends JpaRepository<VerificationCode, Long> {
+
 }

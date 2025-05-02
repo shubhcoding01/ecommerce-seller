b3c66fad920f56a6b97d9f629f50b532fc9adebe
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-02 11:00:09 +0530

    Update

--- src/main/java/com/ecomproject/controller/AuthController.java
+++ src/main/java/com/ecomproject/controller/AuthController.java
@@ -22,7 +22,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signup")
-    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody SignupRequest req){
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody SignupRequest req) throws Exception {
 
         String jwt=authService.createUser(req);
 

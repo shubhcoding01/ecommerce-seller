e5df2fb717e598a9c8742fff05e1783217da280e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-03 14:08:48 +0530

    Update UserService.java to include a method for updating user details

--- src/main/java/com/ecomproject/service/UserService.java
+++ src/main/java/com/ecomproject/service/UserService.java
@@ -1,4 +1,8 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.User;
+
 public interface UserService {
+    User findUserByJwtToken(String jwt);
+    User findUserByUsername(String username);
 }

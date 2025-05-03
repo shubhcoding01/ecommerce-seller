0f7ee216660aef2a4f1a11e5e99ff62f23b9d0d5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-03 14:10:31 +0530

    Update UserServiceImpl.java

--- src/main/java/com/ecomproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/UserServiceImpl.java
@@ -1,4 +1,20 @@
 package com.ecomproject.service.impl;
 
-public class UserServiceImpl {
+import com.ecomproject.model.User;
+import com.ecomproject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    @Override
+    public User findUserByJwtToken(String jwt) {
+        return null;
+    }
+
+    @Override
+    public User findUserByUsername(String username) {
+        return null;
+    }
 }

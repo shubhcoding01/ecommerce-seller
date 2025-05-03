cb0a09427ef2313d81c002f528e3b9d42164f169
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-03 14:59:34 +0530

    Update SellerService.java

--- src/main/java/com/ecomproject/service/SellerService.java
+++ src/main/java/com/ecomproject/service/SellerService.java
@@ -1,4 +1,19 @@
 package com.ecomproject.service;
 
+import com.ecomproject.domain.AccountStatus;
+import com.ecomproject.model.Seller;
+
+import java.util.List;
+
 public interface SellerService {
+
+    Seller getSellerProfile(String jwt);
+    Seller createSeller(Seller seller);
+    Seller getSellerById(Long id);
+    Seller getSellerByEmail(String email);
+    List<Seller> getAllSellers(AccountStatus status);
+    Seller updateSeller(Long id,Seller seller);
+    void deleteSeller(Long id);
+    Seller verifySeller(String email, String otp);
+    Seller updateSellerAccountStatus(Long sellerId, AccountStatus status);
 }

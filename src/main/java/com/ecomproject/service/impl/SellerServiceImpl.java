6a8a12c4692c103fecaa1ddbb75dfeb0ae351484
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-03 15:01:12 +0530

    Update SellerServiceImpl.java to add a new method to get all sellers

--- src/main/java/com/ecomproject/service/impl/SellerServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/SellerServiceImpl.java
@@ -1,4 +1,55 @@
 package com.ecomproject.service.impl;
 
-public class SellerServiceImpl {
+import com.ecomproject.domain.AccountStatus;
+import com.ecomproject.model.Seller;
+import com.ecomproject.service.SellerService;
+
+import java.util.List;
+
+
+public class SellerServiceImpl implements SellerService {
+    @Override
+    public Seller getSellerProfile(String jwt) {
+        return null;
+    }
+
+    @Override
+    public Seller createSeller(Seller seller) {
+        return null;
+    }
+
+    @Override
+    public Seller getSellerById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Seller getSellerByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> getAllSellers(AccountStatus status) {
+        return List.of();
+    }
+
+    @Override
+    public Seller updateSeller(Long id, Seller seller) {
+        return null;
+    }
+
+    @Override
+    public void deleteSeller(Long id) {
+
+    }
+
+    @Override
+    public Seller verifySeller(String email, String otp) {
+        return null;
+    }
+
+    @Override
+    public Seller updateSellerAccountStatus(Long sellerId, AccountStatus status) {
+        return null;
+    }
 }

6b27899c8291df6a6f79e05f5390f34ebee0be41
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-04 11:00:54 +0530

    Update

--- src/main/java/com/ecomproject/controller/SellerController.java
+++ src/main/java/com/ecomproject/controller/SellerController.java
@@ -1,6 +1,12 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.model.VerificationCode;
+import com.ecomproject.repository.VerificationCodeRepository;
+import com.ecomproject.response.AuthResponse;
+import com.ecomproject.service.SellerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sellers")
 public class SellerController {
 
+    private final SellerService sellerService;
+    private final VerificationCodeRepository verificationCodeRepository;
+
+    public ResponseEntity<AuthResponse> loginSeller(
+            @RequestBody VerificationCode req
+    ) throws Exception {
+
+        String otp = req.getOtp();
+        String email = req.getEmail();
+
+        VerificationCode verificationCode = verificationCodeRepository.findByEmail(email);
+        if (verificationCode == null || !verificationCode.getOtp().equals(req.getOtp())) {
+            throw new Exception("Wrong Otp...!!!");
+        }
+        
+        return null;
+    }
 }

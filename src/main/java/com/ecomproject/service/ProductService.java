73ee34531d43148f8b5d19aff73cc5c1e9dd3f4a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-05 17:31:11 +0530

    Update

--- src/main/java/com/ecomproject/service/ProductService.java
+++ src/main/java/com/ecomproject/service/ProductService.java
@@ -3,8 +3,29 @@ package com.ecomproject.service;
 import com.ecomproject.model.Product;
 import com.ecomproject.model.Seller;
 import com.ecomproject.request.CreateProductRequest;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 public interface ProductService {
 
     public Product createProduct(CreateProductRequest req, Seller seller);
+    public void deleteProduct(Long productId);
+    public Product updateProduct(Long productId,Product product);
+    Product findProductById(Long productId);
+    List<Product> searchProduct();
+    public Page<Product> getAllProducts(
+            String category,
+            String brand,
+            String colors,
+            String sizes,
+            Integer minPrice,
+            Integer maxPrice,
+            Integer minDiscount,
+            String sort,
+            String stock,
+            Integer pageNumber
+
+
+    );
 }

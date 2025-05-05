f31aae108aaeb381c8b33c2870a1ce524cb4b669
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-05 14:20:49 +0530

    Update

--- src/main/java/com/ecomproject/exceptions/GlobalException.java
+++ src/main/java/com/ecomproject/exceptions/GlobalException.java
@@ -1,8 +1,15 @@
 package com.ecomproject.exceptions;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class GlobalException {
-    
+
+    @ExceptionHandler(SellerException.class)
+    public ResponseEntity<ErrorDetails> sellerExceptionHandler(SellerException se, WebRequest request) {
+            ErrorDetails errorDetails = new ErrorDetails();
+    }
 }

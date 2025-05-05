6ef287670e8748439d804e3ca654bf67efa5fb25
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-05 17:35:19 +0530

    Update

--- src/main/java/com/ecomproject/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/ProductServiceImpl.java
@@ -1,4 +1,46 @@
 package com.ecomproject.service.impl;
 
-public class ProductServiceImpl {
+import com.ecomproject.model.Product;
+import com.ecomproject.model.Seller;
+import com.ecomproject.request.CreateProductRequest;
+import com.ecomproject.service.ProductService;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public Product createProduct(CreateProductRequest req, Seller seller) {
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Long productId) {
+
+    }
+
+    @Override
+    public Product updateProduct(Long productId, Product product) {
+        return null;
+    }
+
+    @Override
+    public Product findProductById(Long productId) {
+        return null;
+    }
+
+    @Override
+    public List<Product> searchProduct() {
+        return List.of();
+    }
+
+    @Override
+    public Page<Product> getAllProducts(String category, String brand, String colors, String sizes, Integer minPrice, Integer maxPrice, Integer minDiscount, String sort, String stock, Integer pageNumber) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProductsBySellerId(String sellerId) {
+        return List.of();
+    }
 }

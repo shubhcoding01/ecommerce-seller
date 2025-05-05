8130f26c8299a28f4023714bc11e0123c186eed7
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-05 14:13:18 +0530

    Update ErrorDetails.java

--- src/main/java/com/ecomproject/exceptions/ErrorDetails.java
+++ src/main/java/com/ecomproject/exceptions/ErrorDetails.java
@@ -1,8 +1,10 @@
 package com.ecomproject.exceptions;
 
+import java.time.LocalDateTime;
+
 public class ErrorDetails {
 
     private String error;
     private String details;
-    
+    private LocalDateTime timestamp;
 }

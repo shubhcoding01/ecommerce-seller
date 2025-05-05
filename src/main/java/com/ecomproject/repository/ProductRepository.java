db9f353d7f71b00866899f2cf37923fdf5b12803
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-05 17:45:43 +0530

    Update

--- src/main/java/com/ecomproject/repository/ProductRepository.java
+++ src/main/java/com/ecomproject/repository/ProductRepository.java
@@ -1,4 +1,8 @@
 package com.ecomproject.repository;
 
-public class ProductRepository {
+import com.ecomproject.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
 }

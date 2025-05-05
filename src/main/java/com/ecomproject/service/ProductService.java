721e329bbdfbaff69ce6c352be906521e137a7e6
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-05 14:47:13 +0530

    Update

--- src/main/java/com/ecomproject/service/ProductService.java
+++ src/main/java/com/ecomproject/service/ProductService.java
@@ -1,8 +1,9 @@
 package com.ecomproject.service;
 
 import com.ecomproject.model.Product;
+import com.ecomproject.model.Seller;
 
 public interface ProductService {
 
-    public Product createProduct(Product product);
+    public Product createProduct(CreateProductRequest req, Seller seller);
 }

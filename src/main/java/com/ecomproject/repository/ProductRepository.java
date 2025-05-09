faf0b45b11697f658205377a3b9dc61e3ce18b39
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-09 22:10:36 +0530

    Update the ProductRepository.java file to include a method for finding products by category

--- src/main/java/com/ecomproject/repository/ProductRepository.java
+++ src/main/java/com/ecomproject/repository/ProductRepository.java
@@ -2,7 +2,9 @@ package com.ecomproject.repository;
 
 import com.ecomproject.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>,
+        JpaSpecificationExecutor<Product> {
 
 }

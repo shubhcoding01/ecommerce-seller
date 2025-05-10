a17317d1254f51dde6c6dea897310d6d819a3e59
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 12:00:58 +0530

    Update ProductController.java

--- src/main/java/com/ecomproject/controller/ProductController.java
+++ src/main/java/com/ecomproject/controller/ProductController.java
@@ -1,7 +1,14 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
+    
+    private final ProductService productService;
 }

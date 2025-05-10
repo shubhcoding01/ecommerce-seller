3513068c7932d76c4e680135364d4c388d8eb87d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 12:51:01 +0530

    Updating src/main/java/com/ecomproject/controller/ProductController.java

--- src/main/java/com/ecomproject/controller/SellerProductController.java
+++ src/main/java/com/ecomproject/controller/SellerProductController.java
@@ -1,9 +1,14 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.model.Product;
 import com.ecomproject.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,5 +17,8 @@ public class SellerProductController {
 
     private final ProductService productService;
 
-    
+    @GetMapping()
+    public ResponseEntity<List<Product>> getProductBySellerId(
+            
+    ) {}
 }

dac7856418fd96cb103e8c10e1851f7158cd58f7
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 12:47:34 +0530

    Update the ProductController.java file to include a new method for updating product details.

--- src/main/java/com/ecomproject/controller/SellerProductController.java
+++ src/main/java/com/ecomproject/controller/SellerProductController.java
@@ -1,5 +1,6 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/sellers/products")
 public class SellerProductController {
+
+    private final ProductService productService;
 
     
 }

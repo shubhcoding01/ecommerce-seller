0147c2d54db3e1f585b8904fc6a5d1a5519aebf6
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 12:08:08 +0530

    Update the ProductController.java file to include a new method for updating product details.

--- src/main/java/com/ecomproject/controller/ProductController.java
+++ src/main/java/com/ecomproject/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.ecomproject.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @GetMapping("{productId}")
     public ResponseEntity<Product> getProductById(
             @PathVariable long productId)
     throws ProductException

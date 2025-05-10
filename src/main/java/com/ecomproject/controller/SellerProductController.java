d5e832bb697957013b12553c199ad77e79efdad1
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 12:44:17 +0530

    Updating src/main/java/com/ecomproject/controller/ProductController.java

--- src/main/java/com/ecomproject/controller/SellerProductController.java
+++ src/main/java/com/ecomproject/controller/SellerProductController.java
@@ -1,9 +1,13 @@
 package com.ecomproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/sellers/products")
 public class SellerProductController {
+
+    
 }

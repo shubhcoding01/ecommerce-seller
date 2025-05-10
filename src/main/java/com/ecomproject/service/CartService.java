d8ecdf4bd6e03133b781cedd51a3d70e9dd616a1
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 18:29:57 +0530

    Update the CartService class to include a method for adding items to the cart

--- src/main/java/com/ecomproject/service/CartService.java
+++ src/main/java/com/ecomproject/service/CartService.java
@@ -1,4 +1,16 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.CartItem;
+import com.ecomproject.model.Product;
+import com.ecomproject.model.User;
+
 public interface CartService {
+
+    public CartItem addCartItem(
+            User user,
+            Product product,
+            String size,
+            int quantity
+    );
+    
 }

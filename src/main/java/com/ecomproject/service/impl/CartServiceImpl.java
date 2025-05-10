d9d01415a864fe61152254f22c8aa50c2c1dbbfd
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-10 18:36:16 +0530

    Update the CartServiceImpl.java file to include the new method

--- src/main/java/com/ecomproject/service/impl/CartServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/CartServiceImpl.java
@@ -1,4 +1,9 @@
 package com.ecomproject.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CartServiceImpl {
 }

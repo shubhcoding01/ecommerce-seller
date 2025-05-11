50d747dcbe8649b0b1aea8b4bff9489977f88a09
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-11 11:59:30 +0530

    Updating src/main/java/com/ecomproject/service/CartItemService.java Updating src/main/java/com/ecomproject/service/impl/CartItemServiceImpl.java

--- src/main/java/com/ecomproject/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/CartItemServiceImpl.java
@@ -2,9 +2,11 @@ package com.ecomproject.service.impl;
 
 import com.ecomproject.model.CartItem;
 import com.ecomproject.service.CartItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CartItemServiceImpl implements CartItemService {
     @Override
     public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) {

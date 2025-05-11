6538e0e908d02c1d4934944d54961b4ee8d5e390
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-11 11:57:55 +0530

    Update the CartItemService.java file to include a method for removing an item from the cart

--- src/main/java/com/ecomproject/service/impl/CartItemServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/CartItemServiceImpl.java
@@ -1,6 +1,21 @@
 package com.ecomproject.service.impl;
 
+import com.ecomproject.model.CartItem;
 import com.ecomproject.service.CartItemService;
 
 public class CartItemServiceImpl implements CartItemService {
+    @Override
+    public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) {
+        return null;
+    }
+
+    @Override
+    public void removeCartItem(Long userId, Long cartItemId) {
+
+    }
+
+    @Override
+    public CartItem findCartItemById(Long id) {
+        return null;
+    }
 }

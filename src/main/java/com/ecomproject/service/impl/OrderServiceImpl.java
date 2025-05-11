e9dbfded1119ede1f2463f61101fab790ddac9ca
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 00:41:03 +0530

    Update OrderService.java

--- src/main/java/com/ecomproject/service/impl/OrderServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/OrderServiceImpl.java
@@ -1,6 +1,39 @@
 package com.ecomproject.service.impl;
 
+import com.ecomproject.model.*;
 import com.ecomproject.service.OrderService;
 
+import java.util.List;
+import java.util.Set;
+
 public class OrderServiceImpl implements OrderService {
+    @Override
+    public Set<Order> createOrders(User user, Address shippingAddress, Cart cart) {
+        return Set.of();
+    }
+
+    @Override
+    public Order findOrderById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> usersOrderHistory(Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> sellersOrder(Long sellerId) {
+        return List.of();
+    }
+
+    @Override
+    public Order updateOrderStatus(Long orderId, OrderStatus orderStatus) {
+        return null;
+    }
+
+    @Override
+    public Order cancelOrder(Long orderId, User user) {
+        return null;
+    }
 }

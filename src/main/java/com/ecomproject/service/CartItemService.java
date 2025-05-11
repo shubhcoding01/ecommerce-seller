1cec5f3b87fc57288f69afadd1081143c00c346b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-11 11:52:27 +0530

    Updating src/main/java/com/ecomproject/service/CartItemService.java

--- src/main/java/com/ecomproject/service/CartItemService.java
+++ src/main/java/com/ecomproject/service/CartItemService.java
@@ -5,4 +5,5 @@ import com.ecomproject.model.CartItem;
 public interface CartItemService {
 
     CartItem updateCartItem(Long userId, Long id, CartItem cartItem);
+    void removeCartItem(Long userId, Long cartItemId);
 }

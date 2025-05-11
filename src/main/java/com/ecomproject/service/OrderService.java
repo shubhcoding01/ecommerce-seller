ea15fe64235ac475a71588706a4bbfd5da6ea5b6
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 00:37:32 +0530

    Update OrderService.java

--- src/main/java/com/ecomproject/service/OrderService.java
+++ src/main/java/com/ecomproject/service/OrderService.java
@@ -1,4 +1,15 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.*;
+
+import java.util.List;
+import java.util.Set;
+
 public interface OrderService {
+
+    Set<Order> createOrders(User user, Address shippingAddress, Cart cart);
+    Order findOrderById(long id);
+    List<Order> usersOrderHistory(Long userId);
+    List<Order> sellersOrder(Long sellerId);
+    Order updateOrderStatus(Long orderId, OrderStatus orderStatus);
 }

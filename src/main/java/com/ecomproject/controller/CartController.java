fcb94eb5e1989102acfe94f059efa9a705008a46
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-11 15:03:42 +0530

    Update the CartController.java file to include a method for removing items from the cart.

--- src/main/java/com/ecomproject/controller/CartController.java
+++ src/main/java/com/ecomproject/controller/CartController.java
@@ -8,6 +8,7 @@ import com.ecomproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,7 @@ public class CartController {
     private final UserService userService;
 
 
+    @GetMapping
     public ResponseEntity<Cart> findUserCartHandler(
             @RequestHeader("Authorization") String jwt) throws Exception{
 

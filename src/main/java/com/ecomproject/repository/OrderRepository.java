3bddb370151124084d2d5e49b58295370cfeffe7
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 00:44:41 +0530

    Update OrderRepository.java to include a method for finding orders by user ID

--- src/main/java/com/ecomproject/repository/OrderRepository.java
+++ src/main/java/com/ecomproject/repository/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.ecomproject.repository;
 
-public interface OrderRepository {
+import com.ecomproject.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }

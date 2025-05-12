afb540e43d5ab6814f468425142c03145784cf6a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 12:48:44 +0530

    Update OrderController.java to include a new endpoint for order status // and a new method to handle order status updates.

--- src/main/java/com/ecomproject/controller/OrderController.java
+++ src/main/java/com/ecomproject/controller/OrderController.java
@@ -1,13 +1,14 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.domain.PaymentMethod;
+import com.ecomproject.model.Address;
 import com.ecomproject.response.PaymentLinkResponse;
+import com.ecomproject.service.CartService;
 import com.ecomproject.service.OrderService;
 import com.ecomproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +17,14 @@ public class OrderController {
 
     private final OrderService orderService;
     private final UserService userService;
+    private final CartService cartService;
 
     @PostMapping
-    public ResponseEntity<PaymentLinkResponse> createOrderHandler()
+    public ResponseEntity<PaymentLinkResponse> createOrderHandler(
+            @RequestBody Address shippingAddress,
+            @RequestParam PaymentMethod paymentMethod,
+            @RequestHeader("Authorization")String jwt)
+        throws Exception {
+
+    }
 }

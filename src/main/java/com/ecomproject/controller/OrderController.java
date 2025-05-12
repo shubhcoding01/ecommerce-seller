a11c3d014e04abd4861afedbc162dc9aa46839cc
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 17:15:26 +0530

    Update the OrderController.java file to include a new endpoint for order cancellation

--- src/main/java/com/ecomproject/controller/OrderController.java
+++ src/main/java/com/ecomproject/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.ecomproject.model.*;
 import com.ecomproject.response.PaymentLinkResponse;
 import com.ecomproject.service.CartService;
 import com.ecomproject.service.OrderService;
+import com.ecomproject.service.SellerService;
 import com.ecomproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,7 @@ public class OrderController {
     private final OrderService orderService;
     private final UserService userService;
     private final CartService cartService;
+    private final SellerService sellerService;
 
     @PostMapping
     public ResponseEntity<PaymentLinkResponse> createOrderHandler(
@@ -86,5 +88,23 @@ public class OrderController {
         User user = userService.findUserByJwtToken(jwt);
         OrderItem orderItem = orderService.findOrderItemById(orderItemId);
         return new ResponseEntity<>(orderItem, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/{orderId}/cancel")
+    public ResponseEntity<Order> cancelOrder(
+            @PathVariable Long orderId,
+            @RequestHeader("Authorization") String jwt)
+        throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Order order = orderService.cancelOrder(orderId,user);
+
+//        Seller seller = sellerService.getSellerById(order.getSellerId());
+//        SellerReport report = sellerReportService.getSellerReport(seller);
+//
+//        report.setCanceledOrders(report.getCanceledOrders() + 1);
+//        report.setTotalRefunds(report.getTotalRefunds() + order.getTotalSellingPrice());
+//        sellerReportService.updateSellerReport(report);
+
+        return ResponseEntity.ok(order);
     }
 }

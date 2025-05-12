d5d80b56e5a1f36d54ffbdc38fd2fa0eb250a9a8
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 11:25:12 +0530

    Update OrderController.java

--- src/main/java/com/ecomproject/controller/OrderController.java
+++ src/main/java/com/ecomproject/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.service.OrderService;
+import com.ecomproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/orders")
 public class OrderController {
-    
+
+    private final OrderService orderService;
+    private final UserService userService;
 }

6e1c77310747381986708214daef1e11124aea48
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-12 11:23:54 +0530

    Update OrderController

--- src/main/java/com/ecomproject/controller/OrderController.java
+++ src/main/java/com/ecomproject/controller/OrderController.java
@@ -1,4 +1,12 @@
 package com.ecomproject.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/orders")
 public class OrderController {
+    
 }

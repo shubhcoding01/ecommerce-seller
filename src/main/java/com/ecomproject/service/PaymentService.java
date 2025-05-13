c7d15f05701079c64c17470abb15084ceec18bc4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-13 10:42:34 +0530

    Update PaymentService.java

--- src/main/java/com/ecomproject/service/PaymentService.java
+++ src/main/java/com/ecomproject/service/PaymentService.java
@@ -1,4 +1,18 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.Order;
+import com.ecomproject.model.PaymentOrder;
+import com.ecomproject.model.User;
+
+import java.util.Set;
+
 public interface PaymentService {
+
+    PaymentOrder createOrder(User user, Set<Order> orders);
+    PaymentOrder getPaymentOrderById(String orderId);
+    PaymentOrder getPaymentOrderByPaymentId(String orderId);
+    Boolean ProceedPaymentOrder(PaymentOrder paymentOrder,
+                                String paymentId,
+                                String paymentLinkId);
+    
 }

6940f7e88218a26e4a3677733b3a84ef9cb62683
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-13 10:45:00 +0530

    Update PaymentService.java

--- src/main/java/com/ecomproject/service/PaymentService.java
+++ src/main/java/com/ecomproject/service/PaymentService.java
@@ -3,6 +3,7 @@ package com.ecomproject.service;
 import com.ecomproject.model.Order;
 import com.ecomproject.model.PaymentOrder;
 import com.ecomproject.model.User;
+import com.razorpay.PaymentLink;
 
 import java.util.Set;
 
@@ -14,5 +15,8 @@ public interface PaymentService {
     Boolean ProceedPaymentOrder(PaymentOrder paymentOrder,
                                 String paymentId,
                                 String paymentLinkId);
-    
+    PaymentLink createRazorpayPaymentLink(User user, Long amount,
+                                          Long orderId);
+    String createStripePaymentLink(User user,
+                                   Long amount, Long orderId);
 }

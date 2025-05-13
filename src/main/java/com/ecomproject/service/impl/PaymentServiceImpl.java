0699de73047d7b5b31023f2dc120e94829d378b4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-13 10:46:58 +0530

    Update PaymentServiceImpl.java

--- src/main/java/com/ecomproject/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/PaymentServiceImpl.java
@@ -1,4 +1,46 @@
 package com.ecomproject.service.impl;
 
-public class PaymentServiceImpl {
+import com.ecomproject.model.Order;
+import com.ecomproject.model.PaymentOrder;
+import com.ecomproject.model.User;
+import com.ecomproject.service.PaymentService;
+import com.razorpay.PaymentLink;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+    
+    @Override
+    public PaymentOrder createOrder(User user, Set<Order> orders) {
+        return null;
+    }
+
+    @Override
+    public PaymentOrder getPaymentOrderById(String orderId) {
+        return null;
+    }
+
+    @Override
+    public PaymentOrder getPaymentOrderByPaymentId(String orderId) {
+        return null;
+    }
+
+    @Override
+    public Boolean ProceedPaymentOrder(PaymentOrder paymentOrder, String paymentId, String paymentLinkId) {
+        return null;
+    }
+
+    @Override
+    public PaymentLink createRazorpayPaymentLink(User user, Long amount, Long orderId) {
+        return null;
+    }
+
+    @Override
+    public String createStripePaymentLink(User user, Long amount, Long orderId) {
+        return "";
+    }
 }

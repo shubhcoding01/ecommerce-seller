b4d7254a0e41a274ee688f19ae9533c89bb02a7a
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:56:22 +0530

    Updated the WishlistServiceImpl

--- src/main/java/com/ecomproject/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/WishlistServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecomproject.service.impl;
 
 import com.ecomproject.service.WishlistService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class WishlistServiceImpl implements WishlistService {
 }

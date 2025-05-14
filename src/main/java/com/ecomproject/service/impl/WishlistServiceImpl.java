0c9d0cedcce844834abc1564f3f7bf73c7c7d234
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:59:14 +0530

    Updated the WishlistServiceImpl.java to include a method for adding a product to the wishlist.

--- src/main/java/com/ecomproject/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/WishlistServiceImpl.java
@@ -3,6 +3,7 @@ package com.ecomproject.service.impl;
 import com.ecomproject.model.Product;
 import com.ecomproject.model.User;
 import com.ecomproject.model.Wishlist;
+import com.ecomproject.repository.WishlistRepository;
 import com.ecomproject.service.WishlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class WishlistServiceImpl implements WishlistService {
+
+    private final WishlistRepository wishlistRepository;
+    
     @Override
     public Wishlist createWishlist(User user) {
         return null;

4b883ed06d06115fc57ed15ae23b24c86d444742
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:41:48 +0530

    Updated the WishlistRepository to include a method for finding all wishlists by user ID.

--- src/main/java/com/ecomproject/repository/WishlistRepository.java
+++ src/main/java/com/ecomproject/repository/WishlistRepository.java
@@ -1,5 +1,6 @@
 package com.ecomproject.repository;
 
+import com.ecomproject.model.Wishlist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WishlistRepository extends JpaRepository<Wishlist, Long> {

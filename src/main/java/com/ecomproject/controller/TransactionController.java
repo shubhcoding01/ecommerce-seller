3b505980ddbcbfc45eb6ce41500b7a19a4c258f5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:23:04 +0530

    Update TransactionController

--- src/main/java/com/ecomproject/controller/TransactionController.java
+++ src/main/java/com/ecomproject/controller/TransactionController.java
@@ -5,6 +5,7 @@ import com.ecomproject.service.SellerService;
 import com.ecomproject.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class TransactionController {
     private final TransactionService transactionService;
     private final SellerService sellerService;
 
+    @GetMapping()
     public ResponseEntity<List<Transaction>> getTransactionBySeller() {}
 }
 

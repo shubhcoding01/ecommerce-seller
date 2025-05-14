04ba9044b19fdbea81bbced4eae3d95ee8791772
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:51:00 +0530

    Update the WishlistService to include a method for adding items to the wishlist

--- src/main/java/com/ecomproject/service/WishlistService.java
+++ src/main/java/com/ecomproject/service/WishlistService.java
@@ -1,5 +1,6 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.Product;
 import com.ecomproject.model.User;
 import com.ecomproject.model.Wishlist;
 
@@ -7,4 +8,5 @@ public interface WishlistService {
 
     Wishlist createWishlist(User user);
     Wishlist getWishlistUserId(User user );
+    Wishlist addProductToWishlist(Wishlist wishlist, Product product);
 }

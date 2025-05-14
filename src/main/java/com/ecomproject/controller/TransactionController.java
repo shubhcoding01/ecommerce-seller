5155a9a977b0e03e2de090451367a1f79cd7bd14
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:12:48 +0530

    Updated TransactionServiceImpl.java to use the new method for transaction processing

--- src/main/java/com/ecomproject/controller/TransactionController.java
+++ src/main/java/com/ecomproject/controller/TransactionController.java
@@ -1,7 +1,9 @@
 package com.ecomproject.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TransactionController {
 }

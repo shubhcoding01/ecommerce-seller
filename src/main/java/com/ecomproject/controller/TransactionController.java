b08afb98a5d1c752cb5455887cbf7fdba97b0c9c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 22:16:17 +0530

    Updated TransactionServiceImpl.java to use the new method for transaction processing

--- src/main/java/com/ecomproject/controller/TransactionController.java
+++ src/main/java/com/ecomproject/controller/TransactionController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping
+@RequestMapping("/api/transactions")
 public class TransactionController {
 }

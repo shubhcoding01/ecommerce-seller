034296575c7daddddeedba126f5a4d37ecb36ff8
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-14 21:56:36 +0530

    Updated TransactionServiceImpl.java to use the new method for transaction processing

--- src/main/java/com/ecomproject/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/TransactionServiceImpl.java
@@ -20,11 +20,15 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public Transaction createTransaction(Order order) {
+
         Seller seller = sellerRepository.findById(order.getSellerId()).get();
+
         Transaction transaction = new Transaction();
+
         transaction.setSeller(seller);
         transaction.setCustomer(order.getUser());
         transaction.setOrder(order);
+
         return null;
     }
 

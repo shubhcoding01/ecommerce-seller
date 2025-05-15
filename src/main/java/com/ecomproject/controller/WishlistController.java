f6b38bed71e747b3c06057d59b7acfc6dc96d6cc
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-15 19:42:03 +0530

    Update the WishlistController.java file to include a new method for adding items to the wishlist.

--- src/main/java/com/ecomproject/controller/WishlistController.java
+++ src/main/java/com/ecomproject/controller/WishlistController.java
@@ -1,8 +1,10 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.model.Wishlist;
 import com.ecomproject.service.UserService;
 import com.ecomproject.service.WishlistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +15,6 @@ public class WishlistController {
 
     private final WishlistService wishlistService;
     private final UserService userService;
+
+    public ResponseEntity<Wishlist> getWishlistByUserId() {}
 }

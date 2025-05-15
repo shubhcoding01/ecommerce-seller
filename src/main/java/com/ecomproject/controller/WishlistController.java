bc12786b12d675838e8d9f4405d61aa471d82025
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-15 19:40:00 +0530

    Updated the WishlistController to include a new endpoint for adding products to the wishlist.

--- src/main/java/com/ecomproject/controller/WishlistController.java
+++ src/main/java/com/ecomproject/controller/WishlistController.java
@@ -1,5 +1,6 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.service.WishlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/wishlist")
 public class WishlistController {
+
+    private final WishlistService wishlistService;
+    
 }

acab3f38f0c39cceccebcb62e9ddc9d27db08820
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-15 19:43:57 +0530

    Updated the WishlistService and WishlistServiceImpl classes to include a method for adding a product to the wishlist.

--- src/main/java/com/ecomproject/controller/WishlistController.java
+++ src/main/java/com/ecomproject/controller/WishlistController.java
@@ -6,6 +6,7 @@ import com.ecomproject.service.WishlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,7 @@ public class WishlistController {
     private final UserService userService;
 
     @GetMapping()
-    public ResponseEntity<Wishlist> getWishlistByUserId() {}
+    public ResponseEntity<Wishlist> getWishlistByUserId(
+            @RequestHeader
+    ) {}
 }

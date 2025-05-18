05d608c6a773dea8f64faba81d962fe0d69fccde
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-18 20:39:35 +0530

    Update the ReviewService class to include a method for retrieving reviews by product ID

--- src/main/java/com/ecomproject/service/ReviewService.java
+++ src/main/java/com/ecomproject/service/ReviewService.java
@@ -1,13 +1,14 @@
 package com.ecomproject.service;
 
 import com.ecomproject.model.Review;
+import com.ecomproject.model.User;
 
 import java.util.List;
 
 public interface ReviewService {
 
     Review createReview(CreateReviewRequest req,
-                        );
+                        User user);
 
     List<Review> getReviewByProductId(Long productId);
 }

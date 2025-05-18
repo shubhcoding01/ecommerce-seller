beb0f9746319ac431b70b7f61cf3feb1033efec3
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-18 20:17:11 +0530

    Update the ReviewRepository to include a method for finding reviews by product ID

--- src/main/java/com/ecomproject/repository/ReviewRepository.java
+++ src/main/java/com/ecomproject/repository/ReviewRepository.java
@@ -1,4 +1,6 @@
 package com.ecomproject.repository;
 
-public interface ReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
 }

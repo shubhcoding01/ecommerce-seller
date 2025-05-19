aa3899591fa9aa7d8b072b7d0c2c32b04ef7c890
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-19 20:25:24 +0530

    Update the ReviewServiceImpl class to include a method for updating a review

--- src/main/java/com/ecomproject/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/ReviewServiceImpl.java
@@ -1,10 +1,35 @@
 package com.ecomproject.service.impl;
 
+import com.ecomproject.model.Product;
+import com.ecomproject.model.Review;
+import com.ecomproject.model.User;
+import com.ecomproject.request.CreateReviewRequest;
 import com.ecomproject.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService {
+    @Override
+    public Review createReview(CreateReviewRequest req, User user, Product product) {
+        return null;
+    }
+
+    @Override
+    public List<Review> getReviewByProductId(Long productId) {
+        return List.of();
+    }
+
+    @Override
+    public Review updateReview(Long reviewId, String reviewText, double rating, Long userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteReview(Long reviewId, Long userId) {
+
+    }
 }

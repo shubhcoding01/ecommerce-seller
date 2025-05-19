a518160d478b1b271aa781da5b3bbbc17f31ffca
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-19 20:21:42 +0530

    Update the ReviewServiceImpl class to include a method for updating a review

--- src/main/java/com/ecomproject/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/ReviewServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ecomproject.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ReviewServiceImpl {
 }

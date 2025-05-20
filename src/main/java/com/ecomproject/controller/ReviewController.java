1b0c023c8824bfbfea88aebda414fd821e74fdb3
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-20 22:43:29 +0530

    Update the ReviewController.java file to include the new method for adding a review.

--- src/main/java/com/ecomproject/controller/ReviewController.java
+++ src/main/java/com/ecomproject/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ReviewController {
+    private final ReviewService reviewService;
 }

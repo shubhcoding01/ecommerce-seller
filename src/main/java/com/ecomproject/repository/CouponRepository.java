cd2bd4ade9ff79f6f8713cf2ed6de4ebc1c0f76d
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-21 18:52:07 +0530

    Update the coupon repository to use Spring Data JPA

--- src/main/java/com/ecomproject/repository/CouponRepository.java
+++ src/main/java/com/ecomproject/repository/CouponRepository.java
@@ -4,4 +4,5 @@ import com.ecomproject.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+    Coupon findByCode(String code);
 }

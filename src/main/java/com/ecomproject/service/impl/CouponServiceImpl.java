39aa04ce0ee6545c65e8ea0b4c0d3a0aacee5ef4
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-21 18:49:31 +0530

    Update the CouponServiceImpl.java file to include the implementation of the CouponService interface.

--- src/main/java/com/ecomproject/service/impl/CouponServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/CouponServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecomproject.service.impl;
 
 import com.ecomproject.service.CouponService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CouponServiceImpl implements CouponService {
 }

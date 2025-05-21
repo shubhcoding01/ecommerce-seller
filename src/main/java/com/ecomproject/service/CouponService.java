29fd0f195ec98581becc8aa67324769f1827dcc8
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-21 18:44:52 +0530

    Update the CouponService.java file to include the new method for applying coupons.

--- src/main/java/com/ecomproject/service/CouponService.java
+++ src/main/java/com/ecomproject/service/CouponService.java
@@ -1,4 +1,9 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.Cart;
+import com.ecomproject.model.User;
+
 public interface CouponService {
+
+    Cart applyCoupon(String code,double orderValue, User user);
 }

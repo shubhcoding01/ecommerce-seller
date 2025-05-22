c449d4fcdf09958ab852c498250f6285b68fd3ba
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-22 20:30:13 +0530

    Update the AdminCouponController.java file

--- src/main/java/com/ecomproject/controller/AdminCouponController.java
+++ src/main/java/com/ecomproject/controller/AdminCouponController.java
@@ -1,9 +1,11 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.model.Cart;
 import com.ecomproject.service.CartService;
 import com.ecomproject.service.CouponService;
 import com.ecomproject.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +17,6 @@ public class AdminCouponController {
     private final CouponService couponService;
     private final UserService userService;
     private final CartService cartService;
+
+    public ResponseEntity<Cart> applyCoupon(Cart cart) {}
 }

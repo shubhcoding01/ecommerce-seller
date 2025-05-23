a6b0f896cdab9c145e38e0e1192d993402507893
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 19:23:07 +0530

    Update the HomeServiceImpl.java file to include a new method for fetching the latest products   from the database. This method should be added to the HomeService interface and implemented in the HomeServiceImpl class. The new method should be named getLatestProducts and should return a list of Product objects. The implementation should include a query to fetch the latest products from the database, sorted by their creation date in descending order.

--- src/main/java/com/ecomproject/service/impl/HomeServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/HomeServiceImpl.java
@@ -1,7 +1,15 @@
 package com.ecomproject.service.impl;
 
+import com.ecomproject.model.Home;
+import com.ecomproject.model.HomeCategory;
 import com.ecomproject.service.HomeService;
+
+import java.util.List;
 
 public class HomeServiceImpl implements HomeService{
 
+    @Override
+    public Home createHomePageData(List<HomeCategory> allCategories) {
+        return null;
+    }
 }

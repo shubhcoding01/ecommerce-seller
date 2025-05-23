53c173ca076e68acd9bb2ef95c683823cefbe4f5
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 18:50:22 +0530

    Update the HomeCategoryServiceImpl.java file to include the new method

--- src/main/java/com/ecomproject/service/impl/HomeCategoryServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/HomeCategoryServiceImpl.java
@@ -41,7 +41,6 @@ public class HomeCategoryServiceImpl implements HomeCategoryService{
                     
                 }
                 
-    
                 return homeCategoryRepository.save(homeCategory);
             }
 

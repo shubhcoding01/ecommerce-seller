dc215b1ef7901bac18e9f7aa1eda926236584f05
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 19:33:42 +0530

    Update the HomeServiceImpl.java file to include a new method for fetching the latest products

--- src/main/java/com/ecomproject/service/impl/HomeServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/HomeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecomproject.service.impl;
 
+import com.ecomproject.domain.HomeCategorySection;
 import com.ecomproject.model.Home;
 import com.ecomproject.model.HomeCategory;
 import com.ecomproject.service.HomeService;
@@ -7,6 +8,7 @@ import com.ecomproject.service.HomeService;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,14 @@ public class HomeServiceImpl implements HomeService{
 
     @Override
     public Home createHomePageData(List<HomeCategory> allCategories) {
+
+        List<HomeCategory> gridCategories = allCategories.stream()
+                .filter(homeCategory ->
+                        homeCategory.getSection() == HomeCategorySection.GRID)
+                .collect(Collectors.toList());
+
+        
+
         return null;
     }
 }

398d11b18bcd9cc0ecbbb8e672d4f88227df125f
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 18:24:53 +0530

    Update the HomeCategoryService.java file to include the new method for fetching categories by ID.

--- src/main/java/com/ecomproject/service/HomeCategoryService.java
+++ src/main/java/com/ecomproject/service/HomeCategoryService.java
@@ -6,5 +6,5 @@ import java.util.List;
 
 public interface HomeCategoryService {
     HomeCategory createHomeCategory(HomeCategory homeCategory);
-    List
+    List<HomeCategory> createCategories(List<HomeCategory> homeCategories);
 }

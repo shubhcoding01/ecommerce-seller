23cdca321c48bc2f0869fd8dda8c29b74b7df007
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 18:30:55 +0530

    Update the HomeCategoryServiceImpl.java file to include the new method for getting all categories

--- src/main/java/com/ecomproject/service/impl/HomeCategoryServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/HomeCategoryServiceImpl.java
@@ -1,4 +1,33 @@
 package com.ecomproject.service.impl;
 
-public class HomeCategoryServiceImpl {
+import java.util.List;
+
+import com.ecomproject.model.HomeCategory;
+import com.ecomproject.service.HomeCategoryService;
+
+public class HomeCategoryServiceImpl implements HomeCategoryService{
+
+    @Override
+    public HomeCategory createHomeCategory(HomeCategory homeCategory) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createHomeCategory'");
+    }
+
+    @Override
+    public List<HomeCategory> createCategories(List<HomeCategory> homeCategories) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCategories'");
+    }
+
+    @Override
+    public HomeCategory updateHomeCategory(HomeCategory homeCategory, Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateHomeCategory'");
+    }
+
+    @Override
+    public List<HomeCategory> getAllHomeCategories() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllHomeCategories'");
+    }
 }

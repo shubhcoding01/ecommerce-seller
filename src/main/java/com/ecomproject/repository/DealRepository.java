465b0513278e3e6d4114e7f04e4c73bcf97f5bbb
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 20:29:12 +0530

    Update the DealRepository.java file to include the new method for fetching deals by category.

--- src/main/java/com/ecomproject/repository/DealRepository.java
+++ src/main/java/com/ecomproject/repository/DealRepository.java
@@ -1,4 +1,7 @@
 package com.ecomproject.repository;
 
-public interface DealRepository {
+import com.ecomproject.model.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DealRepository extends JpaRepository<Deal,Long> {
 }

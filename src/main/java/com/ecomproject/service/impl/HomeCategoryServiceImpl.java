dee8b10040040961a3fedf4559d38b1e30c9380e
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 18:32:41 +0530

    Update the HomeCategoryServiceImpl.java file to include the new method for getting all categories

--- src/main/java/com/ecomproject/service/impl/HomeCategoryServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/HomeCategoryServiceImpl.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import com.ecomproject.model.HomeCategory;
 import com.ecomproject.service.HomeCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class HomeCategoryServiceImpl implements HomeCategoryService{
 
     @Override

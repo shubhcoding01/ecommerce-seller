e8c2bf487498e795884a05679aef57104ab083a3
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 20:34:56 +0530

    Update the HomeServiceImpl.java file to include a new method for fetching the latest products

--- src/main/java/com/ecomproject/service/impl/HomeServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/HomeServiceImpl.java
@@ -46,7 +46,15 @@ public class HomeServiceImpl implements HomeService{
 
         List<Deal> createdDeals = new ArrayList<>();
 
+        if (dealRepository.findAll().isEmpty()){
+            List<Deal> deals = allCategories.stream()
+                    .filter(homeCategory ->
+                            homeCategory.getSection() == HomeCategorySection.DEALS)
+                    .map(homeCategory -> new Deal(null,10,homeCategory))
+                    .collect(Collectors.toList());
+            createdDeals = dealRepository.saveAll(deals);
 
+        }else createdDeals = dealRepository.findAll();
 
         return null;
     }

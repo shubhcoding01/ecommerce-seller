0c00f401c0cb047ee2226472e0e578c3728a931f
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 19:08:22 +0530

    Update the HomeCategoryController class to include a new method for retrieving the list of categories

--- src/main/java/com/ecomproject/controller/HomeCategoryController.java
+++ src/main/java/com/ecomproject/controller/HomeCategoryController.java
@@ -3,10 +3,13 @@ package com.ecomproject.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecomproject.model.Home;
+import com.ecomproject.repository.HomeCategoryRepository;
+import com.ecomproject.service.HomeCategoryService;
 
 import lombok.RequiredArgsConstructor;
 import okhttp3.Response;
@@ -16,6 +19,8 @@ import okhttp3.Response;
 public class HomeCategoryController {
     private final HomeCategoryService homeCategoryService;
     private final HomeCategoryRepository homeCategoryRepository;
+
+    @PostMapping("/home/categories")
     public ResponseEntity<Home> createHomeCategories(
         @RequestBody List<HomeCategory> homeCategories) {
             List<HomeCategory> categories = homeCategoryService.createCategories(homeCategories);

0692febbbe05d1fed46991074699481416a9f8c9
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-23 18:23:43 +0530

    Update HomeCategoryService.java

--- src/main/java/com/ecomproject/service/HomeCategoryService.java
+++ src/main/java/com/ecomproject/service/HomeCategoryService.java
@@ -1,4 +1,10 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.HomeCategory;
+
+import java.util.List;
+
 public interface HomeCategoryService {
+    HomeCategory createHomeCategory(HomeCategory homeCategory);
+    List
 }

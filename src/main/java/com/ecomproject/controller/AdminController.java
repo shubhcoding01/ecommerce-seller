4699b9741efee621bc453b95dbee729d29fbf3da
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-24 17:46:42 +0530

    Update the AdminController.java

--- src/main/java/com/ecomproject/controller/AdminController.java
+++ src/main/java/com/ecomproject/controller/AdminController.java
@@ -1,7 +1,12 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.service.SellerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final SellerService sellerService;
 }

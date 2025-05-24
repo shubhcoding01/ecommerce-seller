d1c4f1319743a0dd7e2cbf5f414f104d2ea4323f
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-24 17:55:50 +0530

    Update the DealService

--- src/main/java/com/ecomproject/service/DealService.java
+++ src/main/java/com/ecomproject/service/DealService.java
@@ -1,4 +1,12 @@
 package com.ecomproject.service;
 
+import com.ecomproject.model.Deal;
+
+import java.util.List;
+
 public interface DealService {
+
+    List<Deal> getDeals();
+    Deal createDeal(Deal deal);
+    
 }

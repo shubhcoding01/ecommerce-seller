d7cba8e0cc4d5a4e3dbe5ea4c7a643f3e1894d7b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-24 18:31:10 +0530

    Update the DealService.java file to include a new method for calculating the total deal value.

--- src/main/java/com/ecomproject/service/DealService.java
+++ src/main/java/com/ecomproject/service/DealService.java
@@ -8,6 +8,6 @@ public interface DealService {
 
     List<Deal> getDeals();
     Deal createDeal(Deal deal);
-    Deal updateDeal(Deal deal);
+    Deal updateDeal(Deal deal,Long id);
     void deleteDeal(Long id) throws Exception;
 }

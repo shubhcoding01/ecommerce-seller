b1df83a28de857b6f2856cb34388c13de019b62b
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-24 18:02:09 +0530

    Update the DealServiceImpl to include a method for retrieving deals by category

--- src/main/java/com/ecomproject/service/impl/DealServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/DealServiceImpl.java
@@ -1,7 +1,30 @@
 package com.ecomproject.service.impl;
 
+import com.ecomproject.model.Deal;
+import com.ecomproject.service.DealService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class DealServiceImpl {
+public class DealServiceImpl implements DealService {
+    @Override
+    public List<Deal> getDeals() {
+        return List.of();
+    }
+
+    @Override
+    public Deal createDeal(Deal deal) {
+        return null;
+    }
+
+    @Override
+    public Deal updateDeal(Deal deal) {
+        return null;
+    }
+
+    @Override
+    public void deleteDeal(Deal deal) {
+
+    }
 }

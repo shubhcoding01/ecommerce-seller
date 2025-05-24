c7f4643800cbbc3f38b25ccfb4e93a29e2111808
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-24 18:25:14 +0530

    Update the DealServiceImpl to include a method for retrieving deals by category

--- src/main/java/com/ecomproject/service/impl/DealServiceImpl.java
+++ src/main/java/com/ecomproject/service/impl/DealServiceImpl.java
@@ -39,7 +39,7 @@ public class DealServiceImpl implements DealService {
     }
 
     @Override
-    public void deleteDeal(Long id) {
+    public void deleteDeal(Long id) throws Exception {
         Deal deal = dealRepository.findById(id).orElseThrow(()->
                 new Exception("Deal Not Found..!!"));
         dealRepository.delete(deal);

241a50bed0b1b755eac052d931c167da01153a5c
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-05-25 10:34:15 +0530

    Update the DealController.java file to include a new method for handling deal creation.

--- src/main/java/com/ecomproject/controller/DealController.java
+++ src/main/java/com/ecomproject/controller/DealController.java
@@ -1,6 +1,10 @@
 package com.ecomproject.controller;
 
+import com.ecomproject.model.Deal;
+import com.ecomproject.service.DealService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/admin/deals")
 public class DealController {
+    private final DealService dealService;
+
+    public ResponseEntity<Deal> createDeals(
+            @RequestBody Deal deals
+    )
 }
